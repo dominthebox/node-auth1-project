8e87ba6998547995c907555101ba4fc4d0547fa6
dominthebox <dsallustro@gmail.com>
2021-09-13 22:19:17 -0400

    wip, register working

--- api/server.js
+++ api/server.js
@@ -7,7 +7,6 @@ const authRouter = require('./auth/auth-router');
 const session = require('express-session');
 const Store = require('connect-session-knex')(session);
 const knex = require('../data/db-config');
-const nodeNotifier = require("node-notifier");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
