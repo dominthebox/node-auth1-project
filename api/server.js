1cac8f22de84167cf7a74d3b54daf6d913336071
dominthebox <dsallustro@gmail.com>
2021-09-13 22:12:01 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const cors = require("cors");
 
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+const knex = require('../data/db-config');
+const nodeNotifier = require("node-notifier");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -19,7 +23,25 @@ const authRouter = require('./auth/auth-router');
  */
 
 const server = express();
-
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'shh',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+    // sameSite: none, 
+  }
+}))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
